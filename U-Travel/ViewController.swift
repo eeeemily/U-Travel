56ff47e45c62b9b351f112e3d10f7133571b44ba
Minghui Zheng <mzheng9@u.rochester.edu>
2022-04-10 13:23:34 -0400

    Initial Commit

---
+++ U-Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  U-Travel
+//
+//  Created by Zheng, Minghui on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
